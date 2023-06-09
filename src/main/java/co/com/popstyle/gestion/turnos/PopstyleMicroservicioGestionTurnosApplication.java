2afff8f9bf4ebae7a69829fa04f4b1760044f342
Harbey Brieceno <harbeybriceno329728@correo.itm.edu.co>
2023-06-08 19:50:52 -0500

    Inicio Micro Servicio Gestion de Turnos

---
+++ src/main/java/co/com/popstyle/gestion/turnos/PopstyleMicroservicioGestionTurnosApplication.java
@@ -0,0 +1,13 @@
+package co.com.popstyle.gestion.turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopstyleMicroservicioGestionTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopstyleMicroservicioGestionTurnosApplication.class, args);
+	}
+
+}
